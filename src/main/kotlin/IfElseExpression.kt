a139acdcb4c3e21c245a0dd3282eb516c47fb284
droid-lover <ersachin20@gmail.com>
2022-03-17 17:25:04 +0530

    updated conditions

--- src/main/kotlin/IfElseExpression.kt
+++ src/main/kotlin/IfElseExpression.kt
@@ -1,11 +1,14 @@
 fun main() {
 //Conditional statements
-// if else expression
+// if else expression (replacement for ternary operator in Kotlin)
 
-    val number = -10
-    if (number > 0) {
-        println("Number is greater than 0")
-    }else{
-        println("Number is not greater than 0")
+//    val number = 4
+    val number = 5
+    val result = if (number %2 == 0) {
+        "EVEN"
+    } else {
+        "ODD"
     }
+
+    println("Number $number is $result")
 }
--- src/main/kotlin/WhenExpression.kt
+++ src/main/kotlin/WhenExpression.kt
@@ -2,7 +2,7 @@ fun main() {
 //Conditional statements
 // when expression ladder(multiple if else statements)
 
-    val temperature = 20
+    val temperature = 34
     val clothesToBuy = when {
         temperature>0 && temperature<=10 -> {
             "Winter coats"
