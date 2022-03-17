0d6c35dcc5e5c36e82dfaafabf42a438150b88f4
droid-lover <ersachin20@gmail.com>
2022-03-17 15:34:02 +0530

    different data types in kotlin

---
+++ src/main/kotlin/DataTypes.kt
@@ -0,0 +1,15 @@
+import com.sun.org.apache.xpath.internal.operations.Bool
+
+//Data Types
+fun main(){
+
+    val x : Int = 20
+
+    val y :Long = 1234L
+
+    val z : Double = 5.6
+
+    val a : Float = 3.4F
+
+    val s :Boolean = true
+}
