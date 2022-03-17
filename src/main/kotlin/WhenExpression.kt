4318eca7ef3283efc9d4b399a305ecf190c3be5a
droid-lover <ersachin20@gmail.com>
2022-03-17 17:15:53 +0530

    When expression inside kotlin

---
+++ src/main/kotlin/WhenExpression.kt
@@ -0,0 +1,22 @@
+fun main() {
+//Conditional statements
+// when expression ladder(multiple if else statements)
+
+    val temperature = 20
+    val clothesToBuy = when {
+        temperature>0 && temperature<=10 -> {
+            "Winter coats"
+        }
+        temperature>10 && temperature<=20 -> {
+            "Jackets"
+        }
+        temperature>20 && temperature<=30 -> {
+            "Shirts"
+        }
+        else -> {
+            "Tshirts"
+        }
+    }
+
+    println("You need to purchase $clothesToBuy")
+}
