64ee2852e6de38a4958cca35f820ca244cabff61
droid-lover <ersachin20@gmail.com>
2022-03-17 15:46:37 +0530

    Pairs in kotlin

---
+++ src/main/kotlin/Pairs.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    //Pairs
+    val studentDetails = Pair("Ron", "Weasely")
+
+    println(studentDetails.first)
+    println(studentDetails.second)
+
+    val oddNumber = Pair<String, Int>("Odd", 1)
+    val evenNumber = Pair("Even", 2)
+
+    println(oddNumber.first)
+    println(oddNumber.second)
+}
