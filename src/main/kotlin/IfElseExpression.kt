1d5c74211b61d1a99bae79491aad01ba1fd2152f
droid-lover <ersachin20@gmail.com>
2022-03-17 15:58:13 +0530

    If else expression implementation

---
+++ src/main/kotlin/IfElseExpression.kt
@@ -0,0 +1,11 @@
+fun main() {
+//Conditional statements
+// if else expression
+
+    val number = -10
+    if (number > 0) {
+        println("Number is greater than 0")
+    }else{
+        println("Number is not greater than 0")
+    }
+}
