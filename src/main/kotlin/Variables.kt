51f389532fc7e6408909965c444a64c6e6084260
droid-lover <ersachin20@gmail.com>
2022-03-17 15:32:06 +0530

    How to use var & val in Kotlin

---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,21 @@
+fun main() {
+
+    //Immutable cant change
+    val x = 10
+
+    //Mutable can change later
+    var y = 6
+    y = 7
+    y += 9
+
+
+    //in val we cant update values later
+//    val name = "Harry"
+//    name = "Harry Potter"
+
+    var name = "harry"
+    name = "harry potter"
+
+    println(name)
+
+}
