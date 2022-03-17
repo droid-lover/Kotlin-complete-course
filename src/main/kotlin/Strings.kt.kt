f45686d0297bb9d1080f88a0263361ead680e442
droid-lover <ersachin20@gmail.com>
2022-03-17 15:41:53 +0530

    Strings in kotlin with memeber functions and string concatenation

---
+++ src/main/kotlin/Strings.kt.kt
@@ -0,0 +1,16 @@
+//Strings
+fun main(){
+
+    val avenger = "Ironman"
+    println(avenger.length)
+    println(avenger[0])
+
+    val quote = "I love you 3000"
+//    val result = avenger + quote
+
+    //with string concatenation
+    val result = "My favourite avenger is $avenger and $quote"
+    println(result)
+
+
+}
