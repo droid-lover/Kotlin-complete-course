a8ed1d15ce0b9a766f9d67c9b57dfcd97b3c6d52
droid-lover <ersachin20@gmail.com>
2022-03-18 13:17:13 +0530

    Nullable types and how we implement these

---
+++ src/main/kotlin/NullableTypes.kt
@@ -0,0 +1,32 @@
+//Null Safety and Nullable Types
+fun main() {
+
+   var name :String = "Apple"
+//   println(name.length)
+
+    var fruit :String? = "mango"
+    fruit = null
+    println(fruit?.length)
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
