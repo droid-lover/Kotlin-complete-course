110fea51ba989bbdf6bfa47a0718bb963de868b5
droid-lover <ersachin20@gmail.com>
2022-03-18 14:54:08 +0530

    Elvis operator implementation code

--- src/main/kotlin/NullableTypes.kt
+++ src/main/kotlin/NullableTypes.kt
@@ -4,15 +4,16 @@ fun main() {
 
     var car :String? = null
 
-    car = "bmw"
-//    if( car != null){
+    //traditional way
+//    if(car != null){
 //        println("car name is $car")
 //    }else{
 //        println("car name is null")
 //    }
 
-    //Safe calls
-    println("car name is $car")
+    //Using elvis operator
+    car = "Audi"
+    println( car ?: "Null" )
 
 
 
