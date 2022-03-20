19a43a7da39859076827f3df94b26f99a672b561
droid-lover <ersachin20@gmail.com>
2022-03-20 18:33:35 +0530

    for loop inside arrays

--- src/main/kotlin/Arrays.kt
+++ src/main/kotlin/Arrays.kt
@@ -1,14 +1,40 @@
 //Arrays
 fun main(){
 
-    //Int Array
-    val numbers = arrayOf(1,2,3,4,5,6)
+//    //Int Array
+//    val numbers = arrayOf(1,2,3,4,5,6)
+//
+//    for( i in numbers){
+//        println(i)
+//    }
 
-    for( i in numbers){
-        println(i)
+
+    //String Array
+    val superheros = arrayOf("Ironman","spidy","hulk")
+    for ( hero in superheros){
+        println(hero)
     }
 
 
 
 
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+//Float array
+val floatValues = arrayOf(3.4f,1.2f,0.06f,10.0f)
+
+//Any mix elements array
+val mixArray = arrayOf<Any>(1,"two",3.0f,4.0)
+
