28b87b5c2ff0f9967b15639af0ff75d755e6c0f9
droid-lover <ersachin20@gmail.com>
2022-03-20 20:31:47 +0530

    for loop in ranges updtaed examples

--- src/main/kotlin/Ranges.kt
+++ src/main/kotlin/Ranges.kt
@@ -2,15 +2,18 @@
 fun main() {
 
     //closed range
-    val numbers = 0..5
-    //0,1,2,3,4,5
-    for (digit in numbers){
-        println(digit)
-    }
+//    val numbers = 0..5
+//    //0,1,2,3,4,5
+//    for (digit in numbers){
+//        println(digit)
+//    }
 
 
     //half_open range
     val counts = 0 until  5
     //0,1,2,3,4
+    for (x in counts){
+        println(x)
+    }
 
 }
