5bdde4a83ee75f9645c7337ac8cb7032d6b8f706
droid-lover <ersachin20@gmail.com>
2022-03-27 12:47:20 +0530

    implemented interface inside a class

---
+++ src/main/kotlin/CalcExample.kt
@@ -0,0 +1,19 @@
+class CalcExample : MyCalculator , AnotherInterface{
+
+    override fun addition() {
+      println("addition")
+    }
+
+    override fun divison() {
+
+    }
+
+    override fun multiply() {
+
+    }
+
+    override fun randomFunction(x: Int, y: Int) {
+
+    }
+
+}
