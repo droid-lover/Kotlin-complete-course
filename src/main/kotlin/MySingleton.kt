0aedbe06eeee1cd45408bdd6188be9ec85f9285a
droid-lover <ersachin20@gmail.com>
2022-03-27 19:10:10 +0530

    how to implement singleton class

---
+++ src/main/kotlin/MySingleton.kt
@@ -0,0 +1,33 @@
+//Singleton class
+object MySingleton {
+    var x = 10
+
+    fun anyFunction(){
+        print("I am a function")
+    }
+
+}
+
+fun main() {
+
+    MySingleton.x
+    MySingleton.anyFunction()
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
