fea10f62f1deb1f8bfaad8f5915f860327787399
droid-lover <ersachin20@gmail.com>
2022-04-02 14:38:59 +0530

    Different types of collections till now

---
+++ src/main/kotlin/DifferentTypeOfCollections.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    //Arrays
+    val numbers = arrayOf(1,2,3,4,5,6)
+    val superheros = arrayOf("Ironman","spidy","hulk")
+
+
+    //List ArrayList and mutable list
+    val numbersList = listOf(1,2,3,4,5,6,7)
+    val evenNumbers = arrayListOf(2,4,6,8)
+
+    val updatableNumbersList = mutableListOf(1,2,3,4,5,6,7)
+    updatableNumbersList.add(8)
+    updatableNumbersList.add(9)
+    updatableNumbersList.add(0,0)
+
+}
