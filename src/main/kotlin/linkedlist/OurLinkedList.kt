e53b1323f2979ae90064df4d444c8a68765008ae
droid-lover <ersachin20@gmail.com>
2022-04-05 17:24:30 +0530

    Implemented push functionality inside OurLinkedList class

--- src/main/kotlin/linkedlist/OurLinkedList.kt
+++ src/main/kotlin/linkedlist/OurLinkedList.kt
@@ -1,20 +1,32 @@
 package linkedlist
+/**
+ * create by Sachin Rajput
+ * (Native Mobile Bits)
+ */
 
 class OurLinkedList<T> {
 
     private var head: Node<T>? = null
     private var tail: Node<T>? = null
-    private var size :Int = 0
+    private var size: Int = 0
 
-    fun isOurLinkedListEmpty() :Boolean{
-        return size ==0
+    fun isOurLinkedListEmpty(): Boolean {
+        return size == 0
     }
 
     override fun toString(): String {
-        return if(isOurLinkedListEmpty()){
+        return if (isOurLinkedListEmpty()) {
             "Our Linkedlist is empty"
-        }else{
-             head.toString()
+        } else {
+            head.toString()
         }
+    }
+
+    fun pushElement(value: T) {
+        head = Node(value, nextNode = head)
+        if(tail == null){
+            tail = head
+        }
+        size+=1
     }
 }
