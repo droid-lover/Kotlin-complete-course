1fe4841518134d631a57ac020f950d0184cb47a0
droid-lover <ersachin20@gmail.com>
2022-04-08 17:02:11 +0530

    implemented example to showcase use of insert function from our linkedin class

--- src/main/kotlin/linkedlist/InsertExample.kt
+++ src/main/kotlin/linkedlist/InsertExample.kt
@@ -9,6 +9,17 @@ fun main() {
     ourLinkedList.appendElement(20) //10,20
     ourLinkedList.appendElement(30) //10,20,30
 
-    println(ourLinkedList.findNodeAt(givenIndex = 0)?.value)
+//    println( ourLinkedList.findNodeAt(givenIndex = 0)?.value)//10
+
+    val afterNodeReference = ourLinkedList.findNodeAt(givenIndex = 1)
+//    if (afterNodeReference != null) {
+//        ourLinkedList.insertElement(value = 40, afterNode = afterNodeReference)
+//    }
+
+    afterNodeReference?.also {
+        ourLinkedList.insertElement(value = 40, afterNode = it)
+    }
+
+    println(ourLinkedList)
 
 }
