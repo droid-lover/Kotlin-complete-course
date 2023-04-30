91dc759910e483f6f661cf36573a511bc5051e43
droid-lover <ersachin20@gmail.com>
2023-04-30 21:22:08 +0530

    kotlin revision

---
+++ src/main/kotlin/Garage.kt
@@ -0,0 +1,15 @@
+
+fun main(){
+
+    val nameValue = readLine()
+//    val name = "Sachin"
+    val name = nameValue
+    name?.forEachIndexed { index, char ->
+    println("Index =$index Char = $char")
+    }
+
+//    val example = "A2 + B2"
+//    val spannableString = SpannableStringBuilder text se h ye
+
+
+}
