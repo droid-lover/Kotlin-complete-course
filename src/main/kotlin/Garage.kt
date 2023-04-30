d4b95abeb62e85859630fd39ac94fc03096cbe75
droid-lover <ersachin20@gmail.com>
2023-04-30 23:58:20 +0530

    Anagram check

--- src/main/kotlin/Garage.kt
+++ src/main/kotlin/Garage.kt
@@ -1,49 +1,68 @@
 import java.util.TreeMap
 
-fun main(){
+fun main() {
 
     val nameValue = readLine()
 //    val name = "Sachin"
     val name = nameValue
     name?.forEachIndexed { index, char ->
-    println("Index =$index Char = $char")
+        println("Index =$index Char = $char")
     }
 
 //    val example = "A2 + B2"
 //    val spannableString = SpannableStringBuilder text se h ye
 
 
+    val array = arrayOf(1, 2, 3, 4)
+    val mixArray = arrayOf<Any>(1, 2, 3, 4.0, 5f, true, false, "HelloJi", "OmNamhaShivay")
 
-    val array = arrayOf(1,2,3,4)
-    val mixArray= arrayOf<Any>(1,2,3,4.0,5f,true,false,"HelloJi","OmNamhaShivay")
-
-    val array3 = Array(4,{i-> i+1})
-    val array4 = Array(4) {i-> i*2}
+    val array3 = Array(4, { i -> i + 1 })
+    val array4 = Array(4) { i -> i * 2 }
 
     println(array3.toList().toString())
     println(array4.toList().toString())
 
-    val set = setOf(1,2,3,1,2,3)
-    val set1 = hashSetOf(1,2,3,1,2,3)
+    val set = setOf(1, 2, 3, 1, 2, 3)
+    val set1 = hashSetOf(1, 2, 3, 1, 2, 3)
     println(set)
     println(set1)
 
     //set.add(4)
     set1.add(4)
 
-    println(set.elementAt(set.size-1))
+    println(set.elementAt(set.size - 1))
     println(set.elementAt(0))
-    println(set1.elementAt(set.size-1))
+    println(set1.elementAt(set.size - 1))
     println(set1.elementAt(0))
 
 
-    val hashMap = hashMapOf<Char,Int>() //order is not maintained
-    val map = java.util.LinkedHashMap<Char,Int>() //ordered
+    val hashMap = hashMapOf<Char, Int>()
+    val map = java.util.LinkedHashMap<Char, Int>()
     "hashMapS".forEach {
-        hashMap.put(it, "hashMapS".count{char-> char == it})
-        map.put(it, "hashMapS".count{char-> char == it})
+        hashMap.put(it, "hashMapS".count { char -> char == it })
+        map.put(it, "hashMapS".count { char -> char == it })
     }
 
     println(hashMap.toString())
     println(map.toString())
+
+    println("======")
+    println( isAnagram("anagram", "nagaram"))
+}
+
+private fun isAnagram(s: String, t: String): Boolean {
+    if(s.length != t.length) return false
+
+    val hashMapS = hashMapOf<Char,Int>()
+    val hashMapT = hashMapOf<Char,Int>()
+
+    for(i in s.indices){
+        hashMapS[s[i]] = 1+hashMapS.getOrDefault(s[i],0)
+        hashMapT[t[i]]= 1+hashMapT.getOrDefault(t[i],0)
+    }
+
+    hashMapS.keys.forEach {
+        if(hashMapS.get(it) != hashMapT.getOrDefault(it,0)) return false
+    }
+    return true
 }
---
+++ src/main/kotlin/linkedlist/ReverseLinkedList.kt
@@ -0,0 +1,21 @@
+//package linkedlist
+//
+//import java.util.Stack
+//
+//class ReverseLinkedList {
+//}
+//
+//fun main(){
+//
+//    val linkedList = OurLinkedList<Int>()
+//
+//    linkedList.appendElement(10)
+//    linkedList.appendElement(20)
+//    linkedList.appendElement(30)
+//
+//    println(linkedList.toString())
+//}
+//
+//fun reverseLinkedList(linkedList: OurLinkedList<Int>): OurLinkedList<Int> {
+//    val stack = Stack<Int>()
+//}
