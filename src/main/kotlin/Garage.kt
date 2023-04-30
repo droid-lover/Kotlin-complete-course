3bb2cdfa0cf929a9722c8d798e25239e9ea57b3f
droid-lover <ersachin20@gmail.com>
2023-04-30 22:17:08 +0530

    Set and hashSet

--- src/main/kotlin/Garage.kt
+++ src/main/kotlin/Garage.kt
@@ -12,4 +12,23 @@ fun main(){
 //    val spannableString = SpannableStringBuilder text se h ye
 
 
+
+    val array = arrayOf(1,2,3,4)
+    val mixArray= arrayOf<Any>(1,2,3,4.0,5f,true,false,"HelloJi","OmNamhaShivay")
+
+    val array3 = Array(4,{i-> i+1})
+    val array4 = Array(4) {i-> i*2}
+
+    println(array3.toList().toString())
+    println(array4.toList().toString())
+
+    val set = setOf(1,2,3,1,2,3)
+    val set1 = hashSetOf(1,2,3,1,2,3)
+    println(set)
+    println(set1)
+
+    println(set.elementAt(set.size-1))
+    println(set.elementAt(0))
+    println(set1.elementAt(set.size-1))
+    println(set1.elementAt(0))
 }
